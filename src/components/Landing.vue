f43b86907253b37a8e0e0f685ab50f58d622f89e
Ben Kemp <benjokemp@gmail.com>
2021-03-05 16:18:14 +0100

    changing price to descending for consistency

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -32,10 +32,10 @@ export default {
         },
         price: (a, b) => {
           if (a.price < b.price) {
-            return -1;
+            return 1;
           }
           if (a.price > b.price) {
-            return 1;
+            return -1;
           }
           return 0;
         },
